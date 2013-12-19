b0324ee5927809dbdf6a1aa5a278aafc133fef7a
Thomas Kiefhaber <thomaskiefhaber@thomass-mbp.home>
2013-12-18 20:57:41 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tellmeitson::Application.config.secret_key_base = '7c1435132631d935a3fa88c502d5cb85809ad972dc47bc1936993e473f71969769d7497e3563fb05ce522af845ce39b98a77d3f8841e0a59292649149724c6c3'

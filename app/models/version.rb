1f7cea0f812169ab0bead4918a0eb272ef747341
Tom Kiefhaber <tom.kiefhaber@livingsocial.com>
2014-08-13 17:23:33 -0400

    specify bucket name in model

--- app/models/version.rb
+++ app/models/version.rb
@@ -2,7 +2,7 @@ class Version < ActiveRecord::Base
   belongs_to :project
   has_many :comments
   default_scope { order('created_at DESC') }
-  has_attached_file :file
+  has_attached_file :file, bucket: 'loopdin'
   validates_attachment_presence :file
   validates_attachment_content_type :file,
     :content_type => ['video/mp4'],
